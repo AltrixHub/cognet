aeedfb136476241b67941f9b486fe0d524a79abf
altrix <altrix.solvers@gmail.com>
2025-01-25 00:11:49 -0500

    Refactor edge

--- src/edge/slot.rs
+++ src/edge/slot.rs
@@ -1,47 +1,4 @@
-use std::hash::{Hash, Hasher};
-use std::marker::PhantomData;
-use ulid::Ulid;
-
-use crate::EdgeId;
-
-#[derive(Debug, Clone, Copy)]
-pub struct TypedId<T: ?Sized> {
-    id: Ulid,
-    _marker: PhantomData<T>,
-}
-
-impl<T: ?Sized> Default for TypedId<T> {
-    fn default() -> Self {
-        Self {
-            id: Ulid::new(),
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<T: ?Sized> TypedId<T> {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn as_ulid(&self) -> Ulid {
-        self.id
-    }
-}
-
-impl<T: ?Sized> PartialEq for TypedId<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
-}
-
-impl<T: ?Sized> Eq for TypedId<T> {}
-
-impl<T: ?Sized> Hash for TypedId<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-    }
-}
+use crate::{EdgeId, TypedId};
 
 pub type InputSlotId = TypedId<InputSlot>;
 pub type OutputSlotId = TypedId<OutputSlot>;
@@ -58,7 +15,7 @@ pub enum DataType {
     String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Data {
     Number(f32),
     String(String),
