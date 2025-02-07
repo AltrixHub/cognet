5e90b3ab056af041d3ed9fe440668ee5b4bd9211
altrix <altrix.solvers@gmail.com>
2025-02-07 18:54:46 -0500

    Add num_cpus dependency and update WASM thread pool initialization

--- examples/wasm/rust/src/lib.rs
+++ examples/wasm/rust/src/lib.rs
@@ -1,7 +1,14 @@
-use cognet::init_thread_pool;
-use wasm_bindgen::prelude::wasm_bindgen;
+use cognet::{
+    init_thread_pool, num_cpus,
+    wasm_bindgen::{self, prelude::wasm_bindgen, JsValue},
+    wasm_bindgen_futures,
+};
 
 #[wasm_bindgen(start)]
-pub fn main() {
-    init_thread_pool(4);
+pub async fn main() -> Result<(), JsValue> {
+    wasm_bindgen_futures::JsFuture::from(init_thread_pool(num_cpus::get()))
+        .await
+        .expect("Failed to initialize Rayon thread pool");
+
+    Ok(())
 }
